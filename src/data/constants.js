af5579b6ed086d879f19fcf12372d00d5390bdd1
jaga8508 <jagadeesh.ns3007@gmail.com>
2024-08-12 00:16:29 +0530

    resume-link-updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -16,7 +16,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   resume:
-    "https://drive.google.com/file/d/1iz1XDc3GqMvlV4PaZY7vIiHtxwKGzRsD/view",
+    "https://drive.google.com/file/d/1iz1XDc3GqMvlV4PaZY7vIiHtxwKGzRsD/view?usp=sharing",
   linkedin: "https://www.linkedin.com/",
   twitter: "https://twitter.com/",
   insta: "https://www.instagram.com/",
