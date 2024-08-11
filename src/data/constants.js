abc3efc0c86b7c866555b66a0aef8afaf95e41c5
jaga8508 <jagadeesh.ns3007@gmail.com>
2024-08-11 23:56:29 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,237 @@
+import Adobe from '../images/Adobe-Logo.png';
+import Photoshop from '../images/Photoshop-Logo.png';
+import Figma from '../images/Figma-Logo.png';
+import SBL from '../images/SBL-Logo.svg';
+import WDT from  '../images/WDT-LOGO.png';
+import NPR from '../images/npr-arts-logo.png';
+import Velammal from '../images/Velammal-Logo.png';
+
+export const Bio = {
+  name: "Jagadeesh",
+  roles: [
+    "Frontend Developer",
+    "UI/UX Designer",
+    "Wordpress Designer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  resume:
+    "https://drive.google.com/file/d/1iz1XDc3GqMvlV4PaZY7vIiHtxwKGzRsD/view",
+  linkedin: "https://www.linkedin.com/",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Adobe XD",
+        image: Adobe,
+      },
+      {
+        name: "Photoshop",
+        image: Photoshop,
+      },
+      {
+        name: "Figma",
+        image: Figma,
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: SBL,
+    role: "Junior Associate",
+    company: "SBL Knowledge Services pvt ltd",
+    date: "Nov 2020 - Dec 2021",
+    desc: "Expert in converting PSD designs into responsive, pixel-perfect HTML/CSS code, ensuring seamless functionality across all devices and browsers.",
+    skills: [
+      "HTML",
+      "Css",
+      "Photoshop",
+      "Adobe Illustrator",
+    ],
+  },
+  {
+    id: 0,
+    img: WDT,
+    role: "Frontend Developer",
+    company: "wedesigntech pvt ltd",
+    date: "March 2022 - Present",
+    desc: "Experienced in crafting diverse WordPress themes, seamlessly blending design and functionality to create unique, responsive websites.",
+    skills: [
+      "Wordpress",
+      "HTML",
+      "Css",
+      "Javascript Basics",
+      "PHP Basics",
+      "Photoshop",
+      "Adobe Illustrator",
+      "Figma",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: NPR,
+    College: "NPR Arts & Science College",
+    date: "June 2017 - May 2020",
+    percentage: "69.5",
+    desc: "A Bachelor of Science in Mathematics graduate with a strong foundation in advanced mathematical theories, problem-solving, and analytical thinking. Proficient in applying mathematical concepts to real-world scenarios and equipped with skills in statistical analysis and computational methods.",
+    degree: "Bachelor of Science - Mathematics",
+  },
+  {
+    id: 1,
+    img: Velammal,
+    school: "Velammal Matriculation Higher Secondary School",
+    date: "June 2016 - May 2017",
+    percentage: "65.3",
+    desc: "In 12th grade, I completed my studies at Vellamal Matric School, focusing on Science with a specialization in Computer Science. The curriculum integrated both theoretical knowledge and practical skills, preparing me for future academic and career pursuits. ",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Next euv",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/440822500/nexteuv-preview.__large_preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=dcc9c29f6aea47e8e818e7a166480019https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    category: "others",
+    webapp: "https://nexteuv.wpengine.com/",
+  },
+  {
+    id: 0,
+    title: "Pettie",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/449940961/petty-preview.__large_preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=8863e76232c5bfc46b2e4bad1b27b70b",
+    category: "others",
+    webapp: "https://pettie.wpengine.com/",
+  },
+  {
+    id: 1,
+    title: "Aaraa",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/426713237/aarapreview1.__large_preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=5f0cf3a318cb115f03ebe846e86b0b5d",
+    category: "fashion",
+    webapp: "https://aaraa.wpengine.com/",
+  },
+  {
+    id: 2,
+    title: "Indusri",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/470194556/Preview+-+Indusri.png?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=93b887fffab8aa12c60b06f23b5cfb0e",
+    category: "others",
+    webapp: "https://indusri.wpengine.com/",
+  },
+  {
+    id: 3,
+    title: "Ai max",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/455152793/Preview.__large_preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=03b0dc0cb064e78259c236a23c074163",
+    category: "others",
+    webapp: "https://aimax.wpengine.com/",
+  },
+  {
+    id: 10,
+    title: "Printme",
+    image: "https://themeforest.img.customer.envatousercontent.com/files/471875545/Printme%20-%20Preview.__large_preview.png?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=34e92c10080e8e1f4a5872d00543f803",
+    category: "fashion",
+    webapp: "https://wdtprintme.wpengine.com/",
+  },
+  {
+    id: 4,
+    title: "Netlink",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/480749147/image%20(18).__large_preview.png?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=110a68b7aea76dd63f84d9b2f767f2a0",
+    category: "broadcast",
+    webapp: "https://wdtnetlink.wpengine.com/",
+  },
+  {
+    id: 5,
+    title: "GaaGa",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/455008278/Preview.__large_preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=829df63edc6937fd16987d3e15f62415",
+    category: "broadcast",
+    webapp: "https://gaaga.wpengine.com/",
+  },
+  {
+    id: 6,
+    title: "Dada",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/508883626/Preview%20-%20DaDa%20RTL%20Ready.__large_preview.png?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=45dc934dd162c33775055e5b93665682",
+    category: "others",
+    webapp: "https://wdtdada.wpengine.com/",
+  },
+  {
+    id: 7,
+    title: "Goat",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/508883968/Preview%20-%20Goat%20RTL%20Ready.__large_preview.png?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=fc4cb5c060df9b9a66c73582d02e9375",
+    category: "others",
+    webapp: "https://wdtgoat.wpengine.com/",
+  },
+  {
+    id: 8,
+    title: "Sheena",
+    image:
+      "https://themeforest.img.customer.envatousercontent.com/files/508884960/Preview%20_%20Sheena%20RTL%20Ready.__large_preview.png?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=e4c6d738fb3a4cc41f8aea9b6767b448",
+    category: "fashion",
+    webapp: "https://wdtsheena.wpengine.com/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
